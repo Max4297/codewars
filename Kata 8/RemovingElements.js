6a00e9d3d34ec972e8f2db68e6c7b59404fc09d5
Levanov Maxim <levanov.max4297@gmail.com>
2023-03-05 14:45:07 +0200

    added solution Removing Elements

---
+++ Kata 8/RemovingElements.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+function removeEveryOther(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr.splice(i + 1, 1);
+  }
+  return arr;
+}
+
+removeEveryOther(["Keep", "Remove", "Keep", "Remove", "Keep"]);
