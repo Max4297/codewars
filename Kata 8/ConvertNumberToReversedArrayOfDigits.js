1a309844b9a829b8ab1f141ce4a616c335b11e0a
Levanov Maxim <levanov.max4297@gmail.com>
2023-03-09 14:26:28 +0200

    added solution Opposites Attract

--- Kata 8/ConvertNumberToReversedArrayOfDigits.js
+++ Kata 8/ConvertNumberToReversedArrayOfDigits.js
@@ -1,3 +1,10 @@
+/*
+DESCRIPTION:
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example(Input => Output):
+*/
 function digitize(n) {
   return Array.from(String(n), Number).reverse();
 }
